6fac781c9a6259852953df00aa79a73dcce12e05
jgonz812 <jeg924@gmail.com>
2019-11-11 11:29:13 -0800

    Adding load/save methods with their respective unit tests

---
+++ Zenith.Library/Game Object Model/GameModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Zenith.Library
+{
+    interface ISerialization
+    {
+        void Serialize();
+
+        void Deserialize();
+    }
+    class GameModel
+    {
+        private GameModel[] savedGames = new GameModel[5];
+        private string gameName;
+        private List<object> gameObjects;
+        private List<string> gameObjectStrings;
+
+        public GameModel(string name)
+        {
+            this.gameName = name;
+        }
+
+        public string GameName
+        {
+            get { return gameName; }
+            set { this.gameName = value; }
+        }
+
+        public List<object> GameObjects
+        {
+            get { return this.gameObjects; }
+        }
+        public void Load(string filename)
+        {
+            string serializedGameObjects = System.IO.File.ReadAllText(string.Format(@"../../SaveFiles/{0}.txt", filename));
+            List<string> gameObjectStrings = serializedGameObjects.Split(',').ToList();
+            foreach (string obj in gameObjects)
+            {
+                gameObjects.Add(obj.Deserialize());
+            }
+        }
+
+        public void Save(string filename)
+        {
+            
+            // serialize all necessary objects
+            foreach(object x in this.gameObjects) 
+            {
+                string serializedObject = x.Serialize();
+                this.gameObjectStrings.Add(serializedObject);
+            }
+            System.IO.File.WriteAllLines(@"../../SaveFiles/", gameObjectStrings);
+        }
+
+    }
+}
---
+++ Zenith.Library/SerializationUnitTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Zenith.Library
+{
+    [TestFixture]
+    public class SerializationUnitTests
+    {
+        [Test]
+        public void Test_Save()
+        {
+            GameModel g = new GameModel("george");
+            g.Save(g.GameName);
+            g.Load(g.GameName);
+            Assert.IsTrue(g.GameObjects[0].ToString() == g.GameName);
+        }
+
+        [Test]
+        public void Test_Load()
+        {
+            GameModel g = new GameModel("george");
+            g.Load(g.GameName);
+            Assert.IsTrue(g.GameObjects[0].ToString() == g.GameName);
+        }
+    }
+}
