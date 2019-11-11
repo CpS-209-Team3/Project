d1766bc894ef7bbe174c50df19f25fead985cf33
cevan828 <cevan828@students.bju.edu>
2019-11-11 06:03:31 -0500

    Added GameObject.cs and Vector.cs

---
+++ Zenith.Library/Game Object Model/GameObject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zenith.Library.Game_Object_Model
+{
+    abstract class GameObject
+    {
+        protected Vector position, velocity;
+
+
+        public GameObject()
+        {
+            position = new Vector(0, 0);
+            velocity = new Vector(0, 0);
+        }
+    }
+}
---
+++ Zenith.Library/Game Object Model/Vector.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zenith.Library.Game_Object_Model
+{
+    class Vector
+    {
+        double x, y;
+        double angle, magnitude;
+
+        double X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+        double Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+        public static Vector operator +(Vector v1, Vector v2) {
+            return new Vector(v1.x + v2.x, v1.y + v2.y);
+        }
+        public static Vector operator -(Vector v1, Vector v2)
+        {
+            return new Vector(v1.x - v2.x, v1.y - v2.y);
+        }
+
+        public Vector(double arg1, double arg2, bool isPolar = false)
+        {
+            if (isPolar)
+            {
+                this.x = Math.Cos(arg1) * arg2;
+                this.y = Math.Sin(arg1) * arg2;
+                this.magnitude = arg2;
+                this.angle = arg1;
+            }
+            else
+            {
+                this.x = arg1;
+                this.y = arg2;
+                this.angle = Math.Atan2(arg2, arg1);
+                this.magnitude = Math.Sqrt(arg1 * arg1 + arg2 * arg2);
+            }
+        }
+    }
+}
