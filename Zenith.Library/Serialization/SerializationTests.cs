ec8216d36ca1e3d959ac7030c01bc5a7fd1adeed
jeg924 <46009030+jeg924@users.noreply.github.com>
2019-11-13 21:44:06 -0500

    Update SerializationTests.cs

--- Zenith.Library/Serialization/SerializationTests.cs
+++ Zenith.Library/Serialization/SerializationTests.cs
@@ -12,19 +12,25 @@ namespace Zenith.Library
         [Test]
         public void Test_Save()
         {
-            GameModel g = new GameModel("george");
-            g.
-            g.Save(g.GameName);
-            g.Load(g.GameName);
-            Assert.IsTrue(g.GameObjects[0].ToString() == g.GameName);
+            GameModel g = new GameModel("Georgy");
+            Starship s = new Starship();
+            g.GameObjects.Add(s);
+            g.Save("Georgy");
+            g.Quit();
+            g.Load("Georgy");
+            Assert.IsTrue(g.GameObjects[0].Type() == "starship");
+       
         }
 
         [Test]
         public void Test_Load()
         {
-            GameModel g = new GameModel("george");
-            g.Load(g.GameName);
-            Assert.IsTrue(g.GameObjects[0].ToString() == g.GameName);
+            GameModel g = new GameModel("Georgy");
+            g.Load(testfile);
+            Assert.IsTrue(g.GameObjects[0].Serialize() == "testfile");
+            Assert.IsTrue(g.GameObjects[3].Serialize() == "3100");
+            Assert.IsTrue(g.GameObjects[1].Serialize() == "LVL2");
+            Assert.IsTrue(g.GameObjects[6].Type() == Laser);
         }
     }
 }
