0d49c6736a0f468f5722a6622c2de08c18a8f01b
splat433 <scplatt7@gmail.com>
2019-11-20 15:24:40 -0500

    Added Sprite Functions for Xamarin.

---
+++ Zenith.Mobile/Zenith.Mobile/Sprite.cs
@@ -0,0 +1,37 @@
+﻿//This Sprite Class is for the Xamarin Files!!!
+//
+//
+//
+//
+//
+//
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Xamarin.Forms;
+using Zenith.Library;
+
+namespace Zenith.View
+{
+    class Sprite : Image
+    {
+        GameObject gameObject;
+        
+        public GameObject GameObject { get { return gameObject; } }
+
+        public void Update()
+        {
+            Margin = new Thickness(gameObject.Position.X, gameObject.Position.Y, 0, 0);
+        }
+
+        public Sprite(GameObject gameObject)
+        {
+            this.gameObject = gameObject;
+            Source = gameObject.ImageSource;
+        }
+    }
+}
