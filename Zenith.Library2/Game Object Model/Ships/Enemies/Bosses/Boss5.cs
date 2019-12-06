84f15b94d6ba69d5245a387c8c3e85ce5a96ae65
cevan828 <cevan828@students.bju.edu>
2019-12-05 20:48:31 -0500

    Implemented Boss5

--- Zenith.Library2/Game Object Model/Ships/Enemies/Bosses/Boss5.cs
+++ Zenith.Library2/Game Object Model/Ships/Enemies/Bosses/Boss5.cs
@@ -6,20 +6,102 @@ namespace Zenith.Library
 {
     class Boss5 : Enemy
     {
-        Sensor sensor;
-        Vector avoid = new Vector(0, 0);
+        private const double spinSpeed = 0.5;
 
-        public override void ShipLoop()
+        private Sensor sensor;
+        private Vector avoid = new Vector(0, 0);
+        private int nextDamageMarker;
+
+        private Vector goal = new Vector(0, 0);
+        private int minionsLeft = 0;
+
+        public override void OnCollision(GameObject gameObject)
         {
-            if (avoid.Magnitude > 1)
+            if (state != EnemyState.Sway && gameObject is Laser)
             {
-                AddForce(avoid);
-                avoid /= 2;
+                var laser = (Laser)gameObject;
+                if (laser.IsFromPlayer)
+                {
+                    var velocity = laser.Position - position;
+                    velocity.Magnitude = 800;
+                    var newLaser = new Laser(laser.Position, velocity, 40, false);
+                    newLaser.ImageSources.Clear();
+                    newLaser.ImageSources.Add(Util.GetShipSpriteFolderPath("Projectiles\\projectile-red.png"));
+                    World.Instance.AddObject(newLaser);
+                }
             }
             else
             {
-                MoveTo(new Vector(World.Instance.Width * 0.75, World.Instance.Height / 2), 10);
+                base.OnCollision(gameObject);
             }
+        }
+
+        public void OnMinionDeath()
+        {
+            --minionsLeft;
+            if (minionsLeft == 0)
+            {
+                state = EnemyState.Sway;
+                nextDamageMarker = ((health - 1) / 1000) * 1000;
+            }
+        }
+
+        public override void ShipLoop()
+        {
+            switch (state)
+            {
+                case EnemyState.Sway:
+                    if (avoid.Magnitude > 1)
+                    {
+                        AddForce(avoid);
+                        avoid /= 2;
+                    }
+                    else MoveTo(new Vector(World.Instance.Width * 0.75, World.Instance.Height / 2), 10);
+
+                    angle = (World.Instance.Player.Position - position).Angle;
+                    cannon.Fire();
+
+                    if (health <= nextDamageMarker)
+                    {
+                        state = EnemyState.Ram;
+                        clock = 0;
+                    }
+                    break;
+                case EnemyState.Ram:
+                    angle += spinSpeed;
+                    MoveTo(World.Instance.Player.Position, 10);
+                    ++clock;
+                    if (clock >= 600)
+                    {
+                        state = EnemyState.Flee;
+                        goal = new Vector(World.Instance.Width, World.Instance.Height / 2);
+                    }
+                    break;
+                case EnemyState.Flee:
+                    angle += spinSpeed;
+                    MoveTo(goal, 10);
+                    if ((goal - position).Magnitude < 100)
+                    {
+                        state = EnemyState.Pause;
+                        clock = 0;
+                        minionsLeft = 10;
+                    }
+                    break;
+                case EnemyState.Pause:
+                    angle += spinSpeed;
+                    if (clock < 300 && clock % 30 == 0)
+                    {
+                        double angle = Math.PI * (World.Instance.Random.NextDouble() - 0.5) + Math.PI;
+                        double x = Math.Cos(angle) * 100;
+                        double y = Math.Sin(angle) * 100;
+                        var enemy = new Enemy2(position - new Vector(x, y));
+                        enemy.OnDeath = OnMinionDeath;
+                        World.Instance.AddObject(enemy);
+                    }
+                    ++clock;
+                    break;
+            }
+
         }
 
         public void OnSense(GameObject gameObject)
@@ -31,8 +113,18 @@ namespace Zenith.Library
                 {
                     var offset = position - laser.Position;
                     double dist = offset.Magnitude;
-                    offset.Magnitude = 6000000;
-                    avoid =  offset / (dist * dist);
+                    offset.Magnitude = 1400000000;
+                    avoid = offset / (dist * dist);
+
+                    if (avoid.Y < 0 && position.Y < 50)
+                    {
+                        avoid.Y = 100 * mass;
+                    }
+                    if (avoid.Y > 0 && position.Y > World.Instance.Height - 50)
+                    {
+                        avoid.Y = -100 * mass;
+                    }
+
                 }
             }
         }
@@ -47,7 +139,12 @@ namespace Zenith.Library
             health = 4000;
             maxHealth = 4000;
 
-            sensor = new Sensor(this, OnSense, 500);
+            nextDamageMarker = ((health / 1000) - 1) * 1000;
+            state = EnemyState.Sway;
+
+            cannon = new Boss2Cannon(this);
+
+            sensor = new Sensor(this, OnSense, 200);
         }
     }
 }
